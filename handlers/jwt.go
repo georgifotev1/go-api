123f9c2f1fe7e6868fbe6b4c1494243134cb744a
georgifotev1 <georgifotev12@gmail.com>
2024-01-28 09:10:23 +0200

    test commit

--- handlers/jwt.go
+++ handlers/jwt.go
@@ -29,7 +29,6 @@ func VerifyToken(tokenString string) (*jwt.Token, error) {
 	}
 
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
